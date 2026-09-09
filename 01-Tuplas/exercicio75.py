66e7e2dbdb7566e9b3ba728c62979338c26c7663
LucasCiril <lucas.cirilo@academico.ifrn.edu.br>
2026-09-09 10:54:49 -0300

    resolução

--- 01-Tuplas/exercicio75.py
+++ 01-Tuplas/exercicio75.py
@@ -4,3 +4,15 @@
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
 
+
+i = tuple(int(input('Informe um valor: '))for i in range(4))
+print(f'Você digitou os números {i}')
+print(f'O número 9 ocorreu {i.count(9)} vezes.')
+if 3 in i:
+    print(f'O valor 3 apareceu a primeira vez na posição {i.index(3)+1}')
+else:
+    print('O valor 3 não foi informado.')
+print(f'Os valores pares digitados foram:', end='')
+for even in i:
+    if even % 2 == 0:
+        print(even, end=' ')
